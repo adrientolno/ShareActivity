ddd962f8df435353d8b14a854396f3ffcd887f20
Adrien Tolno <tus29878@temple.edu>
2023-11-07 14:45:49 -0500

    I create an intent to sent text, then create the chooser to allow the user to select the share app.

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
@@ -10,11 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         // This view contains the text to share
         val editText = findViewById<EditText>(R.id.editTextText)
 
         // When the user clicks this button, share the text
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+            val textToShare  = editText.text.toString()
+
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT, textToShare)
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent,null)
+            startActivity(shareIntent)
 
         }
     }
